753d63dbed9bca38a82ee98f6772262f8ae021dd
Ricardo Bustamante <ricardo@busta.dev>
2022-08-06 14:24:30 +0200

    Enable clang format, add text to the initialization scene.

--- game/scenes/init_scene.cpp
+++ game/scenes/init_scene.cpp
@@ -4,10 +4,27 @@
 
 #include "init_scene.h"
 #include "hge/camera_component.h"
+#include "hge/assets.h"
+#include "hge/text_renderer_component.h"
+#include "hge/harpia_math.h"
 
 namespace Jam {
     void InitScene::Load(Application *application) {
         auto camera = CreateSimpleOrthoCamera(5);
-        camera->SetClearColor(Color::gray);
+        camera->SetClearColor(Color::darkGray);
+
+        auto fontAtlas = LoadTextureAsset("assets/fonts/pixel.png");
+        fontAtlas->_filter = TextureFilter::Nearest;
+        auto fontShader = LoadShaderAsset("assets/shaders/text.vert", "assets/shaders/text.frag");
+        auto fontMaterial = LoadMaterialAsset(fontShader);
+        fontMaterial->SetTexture(fontAtlas);
+        fontMaterial->SetColor(Color::white);
+        fontMaterial->_transparent = true;
+
+        auto textObject = CreateObject("Text");
+        auto textRenderer = textObject->AddComponent<TextRendererComponent>();
+        textRenderer->SetFontMaterial(fontMaterial, 7, 9);
+        textRenderer->SetText("Choices");
+        textObject->transform.SetPosition({-textRenderer->GetSize().x/2,0});
     }
 } // Jam
